28bc8b5f74960e58fafd388323936b13103651c0
noonworks <noonworks@gmail.com>
2019-12-06 20:21:35 +0900

    Add text.

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,14 @@
 "use strict";
 var ERROR_MSG = "ごめんなさい！エラーです。非対応ブラウザかも？";
 var ERROR_TIMEOUT = 10000;
+var EMPTY_TEXT = { big: "", small: ["", ""] };
+var FONT_NAME = "'Noto Sans JP'";
 var HeattecoGenerator = /** @class */ (function () {
     function HeattecoGenerator() {
         var _this = this;
         this._back_loaded = false;
         this._logo_loaded = false;
+        this._t = EMPTY_TEXT;
         // img
         this._back = new Image();
         this._logo = new Image();
@@ -15,8 +18,11 @@ var HeattecoGenerator = /** @class */ (function () {
             return;
         this._canvas = elm;
         this._ctx = this._canvas.getContext("2d") || undefined;
-        if (!this._ctx)
-            return;
+        if (!this._ctx) {
+            throw new Error(ERROR_MSG);
+        }
+        this._ctx.textBaseline = "top";
+        this._ctx.fillStyle = "#ffffff";
         // load img
         this._back.addEventListener("load", function () {
             _this._back_loaded = true;
@@ -42,31 +48,14 @@ var HeattecoGenerator = /** @class */ (function () {
         enumerable: true,
         configurable: true
     });
-    HeattecoGenerator.prototype.start = function () {
-        var _this = this;
-        if (!this._ctx) {
-            throw new Error(ERROR_MSG);
-        }
-        var interval = 100;
-        var timeout = ERROR_TIMEOUT / interval;
-        var intervalId = setInterval(function () {
-            timeout--;
-            if (_this.loaded) {
-                clearInterval(intervalId);
-                console.log("resources loaded.");
-                _this.draw();
-                return;
-            }
-            if (timeout < 0) {
-                clearInterval(intervalId);
-            }
-        }, interval);
-    };
     HeattecoGenerator.prototype.save = function () {
         if (!this.loaded || !this._canvas)
             return;
         var url = this._canvas.toDataURL();
         console.log(url);
+    };
+    HeattecoGenerator.prototype.setText = function (t) {
+        this._t = t;
     };
     HeattecoGenerator.prototype.draw = function () {
         if (!this.loaded || !this._ctx)
@@ -74,33 +63,118 @@ var HeattecoGenerator = /** @class */ (function () {
         // clear
         this._ctx.drawImage(this._back, 0, 0);
         // [TODO] add picture
-        // [TODO] add text
+        // add text
+        this.drawText();
         // add logo
         this._ctx.drawImage(this._logo, 0, 0);
     };
+    HeattecoGenerator.prototype.drawText = function () {
+        if (!this.loaded || !this._ctx || !this._canvas)
+            return;
+        if (this._t.big.length > 0) {
+            this._ctx.font = "bold 28px " + FONT_NAME;
+            this._ctx.fillText(this._t.big, 16, 66);
+        }
+        if (this._t.small[0].length > 0) {
+            this._ctx.font = "16px " + FONT_NAME;
+            this._ctx.fillText(this._t.small[0], 20, 127);
+        }
+        if (this._t.small[1].length > 0) {
+            this._ctx.font = "16px " + FONT_NAME;
+            this._ctx.fillText(this._t.small[1], 20, 156);
+        }
+    };
     return HeattecoGenerator;
 }());
+var Controler = /** @class */ (function () {
+    function Controler() {
+        var _this = this;
+        this._text = EMPTY_TEXT;
+        var onchangetext = function () {
+            _this.onChangeText();
+        };
+        this._ctl_b = this.getTextInput("bigtext", onchangetext);
+        this._ctl_s1 = this.getTextInput("smalltext1", onchangetext);
+        this._ctl_s2 = this.getTextInput("smalltext2", onchangetext);
+    }
+    Object.defineProperty(Controler.prototype, "loaded", {
+        // private _bigtext: string = "";
+        get: function () {
+            return !(!this._ctl_b || !this._ctl_s1 || !this._ctl_s2);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Controler.prototype, "text", {
+        get: function () {
+            if (!this._ctl_b || !this._ctl_s1 || !this._ctl_s2)
+                return EMPTY_TEXT;
+            return {
+                big: this._ctl_b.value,
+                small: [this._ctl_s1.value, this._ctl_s2.value]
+            };
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Controler.prototype.setCanvas = function (ht) {
+        this._ht = ht;
+        this.onChangeText();
+    };
+    Controler.prototype.onChangeText = function () {
+        if (!this._ctl_b || !this._ctl_s1 || !this._ctl_s2)
+            return;
+        var t = {
+            big: this._ctl_b.value,
+            small: [this._ctl_s1.value, this._ctl_s2.value]
+        };
+        if (this._text.big == t.big &&
+            this._text.small[0] == t.small[0] &&
+            this._text.small[1] == t.small[1])
+            return;
+        this._text = t;
+        if (this._ht) {
+            this._ht.setText(t);
+            this._ht.draw();
+        }
+    };
+    Controler.prototype.getTextInput = function (id, f) {
+        var t = document.getElementById(id);
+        if (t) {
+            t.addEventListener("change", f);
+            t.addEventListener("keyup", f);
+            return t;
+        }
+        return undefined;
+    };
+    return Controler;
+}());
 function initialize() {
-    var ht = new HeattecoGenerator();
+    var ct = new Controler();
+    if (!ct.loaded) {
+        alert(ERROR_MSG);
+        return;
+    }
     try {
-        ht.start();
+        var ht_1 = new HeattecoGenerator();
+        var interval = 100;
+        var timeout_1 = ERROR_TIMEOUT / interval;
+        var intervalId_1 = setInterval(function () {
+            timeout_1--;
+            if (ht_1.loaded) {
+                clearInterval(intervalId_1);
+                ct.setCanvas(ht_1);
+                console.log("initialized.");
+                return;
+            }
+            if (timeout_1 < 0) {
+                clearInterval(intervalId_1);
+                alert(ERROR_MSG);
+            }
+        }, interval);
     }
     catch (error) {
         alert(error);
     }
-    var interval = 100;
-    var timeout = ERROR_TIMEOUT / interval;
-    var intervalId = setInterval(function () {
-        timeout--;
-        if (ht.loaded) {
-            clearInterval(intervalId);
-            console.log("initialized.");
-            return;
-        }
-        if (timeout < 0) {
-            clearInterval(intervalId);
-            alert(ERROR_MSG);
-        }
-    }, interval);
 }
 window.addEventListener("load", initialize);
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 const ERROR_MSG = "ごめんなさい！エラーです。非対応ブラウザかも？";
 const ERROR_TIMEOUT = 10000;
+const EMPTY_TEXT: ITextData = { big: "", small: ["", ""] };
+const FONT_NAME = "'Noto Sans JP'";
 
 class HeattecoGenerator {
   private _canvas?: HTMLCanvasElement;
@@ -8,6 +10,7 @@ class HeattecoGenerator {
   private _back_loaded = false;
   private _logo: HTMLImageElement;
   private _logo_loaded = false;
+  private _t: ITextData = EMPTY_TEXT;
 
   public get loaded(): boolean {
     return this._back_loaded && this._logo_loaded;
@@ -22,7 +25,11 @@ class HeattecoGenerator {
     if (!elm) return;
     this._canvas = elm as HTMLCanvasElement;
     this._ctx = this._canvas.getContext("2d") || undefined;
-    if (!this._ctx) return;
+    if (!this._ctx) {
+      throw new Error(ERROR_MSG);
+    }
+    this._ctx.textBaseline = "top";
+    this._ctx.fillStyle = "#ffffff";
     // load img
     this._back.addEventListener("load", () => {
       this._back_loaded = true;
@@ -42,64 +49,148 @@ class HeattecoGenerator {
     this._logo.src = "img/logo.png";
   }
 
-  public start() {
-    if (!this._ctx) {
-      throw new Error(ERROR_MSG);
-    }
-    const interval = 100;
-    let timeout = ERROR_TIMEOUT / interval;
-    const intervalId = setInterval(() => {
-      timeout--;
-      if (this.loaded) {
-        clearInterval(intervalId);
-        console.log("resources loaded.");
-        this.draw();
-        return;
-      }
-      if (timeout < 0) {
-        clearInterval(intervalId);
-      }
-    }, interval);
-  }
-
   public save() {
     if (!this.loaded || !this._canvas) return;
     const url = this._canvas.toDataURL();
     console.log(url);
   }
 
-  private draw(): void {
+  public setText(t: ITextData) {
+    this._t = t;
+  }
+
+  public draw(): void {
     if (!this.loaded || !this._ctx) return;
     // clear
     this._ctx.drawImage(this._back, 0, 0);
     // [TODO] add picture
-    // [TODO] add text
+    // add text
+    this.drawText();
     // add logo
     this._ctx.drawImage(this._logo, 0, 0);
+  }
+
+  private drawText(): void {
+    if (!this.loaded || !this._ctx || !this._canvas) return;
+    if (this._t.big.length > 0) {
+      this._ctx.font = "bold 28px " + FONT_NAME;
+      this._ctx.fillText(this._t.big, 16, 66);
+    }
+    if (this._t.small[0].length > 0) {
+      this._ctx.font = "16px " + FONT_NAME;
+      this._ctx.fillText(this._t.small[0], 20, 127);
+    }
+    if (this._t.small[1].length > 0) {
+      this._ctx.font = "16px " + FONT_NAME;
+      this._ctx.fillText(this._t.small[1], 20, 156);
+    }
+  }
+}
+
+interface ITextData {
+  big: string;
+  small: string[];
+}
+
+interface IControler {
+  text: ITextData;
+}
+
+class Controler {
+  // text
+  private _ctl_b?: HTMLInputElement;
+  private _ctl_s1?: HTMLInputElement;
+  private _ctl_s2?: HTMLInputElement;
+  private _text: ITextData = EMPTY_TEXT;
+  //
+  private _ht?: HeattecoGenerator;
+  // private _bigtext: string = "";
+
+  public get loaded(): boolean {
+    return !(!this._ctl_b || !this._ctl_s1 || !this._ctl_s2);
+  }
+
+  public get text(): ITextData {
+    if (!this._ctl_b || !this._ctl_s1 || !this._ctl_s2) return EMPTY_TEXT;
+    return {
+      big: this._ctl_b.value,
+      small: [this._ctl_s1.value, this._ctl_s2.value]
+    };
+  }
+
+  constructor() {
+    const onchangetext = () => {
+      this.onChangeText();
+    };
+    this._ctl_b = this.getTextInput("bigtext", onchangetext);
+    this._ctl_s1 = this.getTextInput("smalltext1", onchangetext);
+    this._ctl_s2 = this.getTextInput("smalltext2", onchangetext);
+  }
+
+  public setCanvas(ht: HeattecoGenerator): void {
+    this._ht = ht;
+    this.onChangeText();
+  }
+
+  private onChangeText() {
+    if (!this._ctl_b || !this._ctl_s1 || !this._ctl_s2) return;
+    const t: ITextData = {
+      big: this._ctl_b.value,
+      small: [this._ctl_s1.value, this._ctl_s2.value]
+    };
+    if (
+      this._text.big == t.big &&
+      this._text.small[0] == t.small[0] &&
+      this._text.small[1] == t.small[1]
+    )
+      return;
+    this._text = t;
+    if (this._ht) {
+      this._ht.setText(t);
+      this._ht.draw();
+    }
+  }
+
+  private getTextInput(
+    id: string,
+    f: () => void
+  ): HTMLInputElement | undefined {
+    const t = document.getElementById(id);
+    if (t) {
+      t.addEventListener("change", f);
+      t.addEventListener("keyup", f);
+      return t as HTMLInputElement;
+    }
+    return undefined;
   }
 }
 
 function initialize() {
-  const ht = new HeattecoGenerator();
+  const ct = new Controler();
+  if (!ct.loaded) {
+    alert(ERROR_MSG);
+    return;
+  }
   try {
-    ht.start();
+    const ht = new HeattecoGenerator();
+    const interval = 100;
+    let timeout = ERROR_TIMEOUT / interval;
+    const intervalId = setInterval(() => {
+      timeout--;
+      if (ht.loaded) {
+        clearInterval(intervalId);
+        ct.setCanvas(ht);
+        console.log("initialized.");
+        return;
+      }
+      if (timeout < 0) {
+        clearInterval(intervalId);
+        alert(ERROR_MSG);
+      }
+    }, interval);
   } catch (error) {
     alert(error);
   }
-  const interval = 100;
-  let timeout = ERROR_TIMEOUT / interval;
-  const intervalId = setInterval(() => {
-    timeout--;
-    if (ht.loaded) {
-      clearInterval(intervalId);
-      console.log("initialized.");
-      return;
-    }
-    if (timeout < 0) {
-      clearInterval(intervalId);
-      alert(ERROR_MSG);
-    }
-  }, interval);
 }
 
 window.addEventListener("load", initialize);
